441d1f58c7732de16be26c0fb0eafb4b5cde7ab8
Gururajkondaguli <kondaguligururaj@gmail.com>
2025-11-18 08:17:08 +0530

    Build for GitHub Pages (docs folder)

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Portfolio-React-Vite/', 
+  base: '/Portfolio-React-Vite/',   // ✅ repo name
+  build: {
+    outDir: 'docs',                 // 👈 build output will go to docs/
+  },
 })
